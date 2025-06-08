bcec16709725e8f8d84794ce96a293ed80c980f1
Themistoklis Paraschiakos <119680912+thp42@users.noreply.github.com>
2025-06-09 00:50:17 +0200

    Update feature_processing.py

--- alphafold/data/feature_processing.py
+++ alphafold/data/feature_processing.py
@@ -61,7 +61,7 @@ def pair_and_merge(
 
   np_chains_list = list(all_chain_features.values())
 
-  pair_msa_sequences = not _is_homomer_or_monomer(np_chains_list
+  pair_msa_sequences = not _is_homomer_or_monomer(np_chains_list)
   #This is my modified version
   pair_msa_sequences = False
             
