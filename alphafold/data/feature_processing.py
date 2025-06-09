9d39cf143c497c71e7d5a8ed85d86c887c21953c
Themistoklis Paraschiakos <119680912+thp42@users.noreply.github.com>
2025-06-09 13:36:38 +0200

    Update feature_processing.py

--- alphafold/data/feature_processing.py
+++ alphafold/data/feature_processing.py
@@ -118,7 +118,7 @@ def _crop_single_chain(chain: pipeline.FeatureDict,
 
   if pair_msa_sequences:
     msa_size_all_seq = chain['num_alignments_all_seq']
-    msa_crop_size_all_seq = np.minimum(msa_size_all_seq, 2 // 2)
+    msa_crop_size_all_seq = np.minimum(msa_size_all_seq, msa_crop_size // 2)
 
     # We reduce the number of un-paired sequences, by the number of times a
     # sequence from this chain's MSA is included in the paired MSA.  This keeps
